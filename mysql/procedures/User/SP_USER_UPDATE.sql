f9ee4066d3260adf71c56df25fd6bdfc345a695d
Abraham Salazar <abraham_jack10@hotmail.com>
2021-11-05 12:07:04 -0500

    Create SP_USER_UPDATE.sql

---
+++ mysql/procedures/User/SP_USER_UPDATE.sql
@@ -0,0 +1,21 @@
+DROP PROCEDURE IF EXISTS SP_USER_UPDATE;
+DELIMITER $
+CREATE PROCEDURE SP_USER_UPDATE(
+    IN _id INT,
+    IN _username VARCHAR(64),
+    IN _password CHAR(32),
+    IN _fisrtName VARCHAR(64),
+    IN _lastName VARCHAR(64),
+    IN _documentNumber VARCHAR(64),
+    IN _active BOOLEAN)
+BEGIN
+    UPDATE User SET
+        Username = _username,
+        Password = _password,
+        FirstName = _fisrtName,
+        LastName = _lastName,
+        DocumentNumber = _documentNumber,
+        Active = _active
+    WHERE Id = _id;
+END $
+DELIMITER ;
