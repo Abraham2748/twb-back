229ea8dad080e84129246db0c65cfd95ada58002
Abraham Salazar <abraham_jack10@hotmail.com>
2021-11-08 18:17:05 -0500

    fix: variable rename

--- intranet/auth/login.php
+++ intranet/auth/login.php
@@ -9,9 +9,9 @@ $responses = new Responses();
 header('Content-type: application/json');
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $postBody = file_get_contents("php://input");
-    $data = $_responses->ok('correct method');
+    $data = $responses->ok('correct method');
 } else {
-    $data = $_responses->error_405();
+    $data = $responses->error_405();
 }
 
 echo json_encode($data);
