7eb7c6f0528b15ebcc4fcab372b9ce752c7aedd3
Abraham Salazar <abraham_jack10@hotmail.com>
2021-11-08 18:07:15 -0500

    Testing correct method on login

--- intranet/auth/login.php
+++ intranet/auth/login.php
@@ -5,3 +5,13 @@ require_once('./responses.php');
 
 $connection = new Connection();
 $responses = new Responses();
+
+header('Content-type: application/json');
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $postBody = file_get_contents("php://input");
+    $data = $_responses->ok('correct method');
+} else {
+    $data = $_responses->error_405();
+}
+
+echo json_encode($data);
