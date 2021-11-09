b00ff40f190f368b60b4407fc564a6da54e561d6
Abraham Salazar <abraham_jack10@hotmail.com>
2021-11-08 22:25:14 -0500

    fix: Spelling

--- mysql/procedures/User/SP_USER_ADD.sql
+++ mysql/procedures/User/SP_USER_ADD.sql
@@ -3,7 +3,7 @@ DELIMITER $
 CREATE PROCEDURE SP_USER_ADD(
     IN _username VARCHAR(64),
     IN _password CHAR(32),
-    IN _fisrtName VARCHAR(64),
+    IN _firstName VARCHAR(64),
     IN _lastName VARCHAR(64),
     IN _documentNumber VARCHAR(64))
 BEGIN
@@ -17,7 +17,7 @@ BEGIN
     VALUES (
         _username,
         _password,
-        _fisrtName,
+        _firstName,
         _lastName,
         _documentNumber,
         1);
