caba719ba319fb210e0b1f446e3291df1d2efdee
Abraham Salazar <abraham_jack10@hotmail.com>
2021-11-08 20:09:42 -0500

    feat: User/index.php Initial implementation

--- intranet/user/index.php
+++ intranet/user/index.php
@@ -1,3 +1,32 @@
 <?php
 
-echo 'user/index.php works!';
+require_once('../../connection.php');
+require_once('../../responses.php');
+
+$connection = new Connection();
+$responses = new Responses();
+
+header('Content-type: application/json');
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'POST':
+        $postBody = json_decode(file_get_contents("php://input"), true);
+        $data = $responses->ok($postBody);
+        break;
+    case 'GET':
+        $postBody = json_decode(file_get_contents("php://input"), true);
+        $data = $responses->ok($postBody);
+        break;
+    case 'PUT':
+        $postBody = json_decode(file_get_contents("php://input"), true);
+        $data = $responses->ok($postBody);
+        break;
+    case 'DELETE':
+        $postBody = json_decode(file_get_contents("php://input"), true);
+        $data = $responses->ok($postBody);
+        break;
+    default:
+        $data = $responses->error_405();
+        break;
+}
+
+echo json_encode($data);
