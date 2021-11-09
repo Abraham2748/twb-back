ad7ca1b3a93ad72828f010961c899732d8d0b676
Abraham Salazar <abraham_jack10@hotmail.com>
2021-11-08 21:56:34 -0500

    fix: Refactor postBody

--- intranet/user/index.php
+++ intranet/user/index.php
@@ -7,21 +7,18 @@ $connection = new Connection();
 $responses = new Responses();
 
 header('Content-type: application/json');
+$postBody = json_decode(file_get_contents("php://input"), true);
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'POST':
-        $postBody = json_decode(file_get_contents("php://input"), true);
         $data = $responses->ok($postBody);
         break;
     case 'GET':
-        $postBody = json_decode(file_get_contents("php://input"), true);
         $data = $responses->ok($postBody);
         break;
     case 'PUT':
-        $postBody = json_decode(file_get_contents("php://input"), true);
         $data = $responses->ok($postBody);
         break;
     case 'DELETE':
-        $postBody = json_decode(file_get_contents("php://input"), true);
         $data = $responses->ok($postBody);
         break;
     default:
