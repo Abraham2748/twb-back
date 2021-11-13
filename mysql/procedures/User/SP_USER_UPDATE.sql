a45540045ca05b88296f90a96a32aacfa9347009
Abraham Salazar <abraham_jack10@hotmail.com>
2021-11-12 22:27:37 -0500

    feat: Add Gender field to SP_USER_UPDATE

--- mysql/procedures/User/SP_USER_UPDATE.sql
+++ mysql/procedures/User/SP_USER_UPDATE.sql
@@ -2,6 +2,7 @@ DROP PROCEDURE IF EXISTS SP_USER_UPDATE;
 DELIMITER $
 CREATE PROCEDURE SP_USER_UPDATE(
     IN _id INT,
+    IN _idGender INT,
     IN _username VARCHAR(64),
     IN _password CHAR(32),
     IN _firstName VARCHAR(64),
@@ -9,6 +10,7 @@ CREATE PROCEDURE SP_USER_UPDATE(
     IN _documentNumber VARCHAR(64))
 BEGIN
     UPDATE User SET
+        Id_Gender = _idGender,
         Username = _username,
         Password = _password,
         FirstName = _firstName,
