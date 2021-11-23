36f55ace495d14b8d453ca8086542ec6fb17b13e
Abraham Salazar <abraham_jack10@hotmail.com>
2021-11-22 21:14:43 -0500

    Create SP_PRODUCT_CATEGORY_UPDATE.sql

---
+++ mysql/procedures/ProductCategory/SP_PRODUCT_CATEGORY_UPDATE.sql
@@ -0,0 +1,13 @@
+DROP PROCEDURE IF EXISTS SP_PRODUCT_CATEGORY_UPDATE;
+DELIMITER $
+CREATE PROCEDURE SP_PRODUCT_CATEGORY_UPDATE(
+    IN _id INT,
+    IN _name VARCHAR(64),
+    IN _photo LONGTEXT)
+BEGIN
+    UPDATE User SET
+        Name = _name,
+        Photo = _photo
+    WHERE Id = _id;
+END $
+DELIMITER ;
